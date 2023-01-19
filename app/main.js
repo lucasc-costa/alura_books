a09ecb2babaac77e808a7ad1f4d15af12be9ccbe
lucasc-costa <62854155+lucasc-costa@users.noreply.github.com>
2023-01-19 13:39:25 -0300

    função map

--- app/main.js
+++ app/main.js
@@ -1,9 +1,12 @@
 let livros = []
 const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 getBuscarLivrosDaAPI()
+const elementoParaInserirLivros = document.getElementById('livros')
 
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI)
     livros = await res.json()
-    console.table(livros);
+    let livrosComDesconto = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
 }
+
